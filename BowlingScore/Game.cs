730b5a92f4a46cbbf5e5775d9762adaff227a2ba
gunnerr91 <blissvortex@hotmail.com>
2021-01-09 08:56:03 +0000

    added spare count bonus to total score

--- BowlingScore/Game.cs
+++ BowlingScore/Game.cs
@@ -1,10 +1,54 @@
-﻿namespace BowlingScore
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingScore
 {
     public class Game
     {
+        private List<int> scoreSet = new List<int>();
+        private List<int> currentFrameScores = new List<int>();
+        private Dictionary<int, int> scoreSetv2 = new Dictionary<int, int>();
+        private int currentFrame = 1;
+        private bool spareBonusActive = false;
+
+        public void Roll(int score)
+        {
+            scoreSet.Add(score);
+
+            currentFrameScores.Add(score);
+
+            if (spareBonusActive)
+            {
+                scoreSetv2[currentFrame - 1] += score;
+                spareBonusActive = false;
+            }
+
+            if (currentFrameScores.Sum() == 10)
+            {
+                spareBonusActive = true;
+            }
+
+
+            if (currentFrameScores.Count > 1)
+            {
+
+                scoreSetv2.Add(currentFrame, currentFrameScores.Sum());
+                currentFrame++;
+                currentFrameScores.Clear();
+            }
+        }
+
         public int Score()
         {
-            return 0;
+            var totalScore = 0;
+
+            foreach (var s in scoreSetv2)
+            {
+                totalScore += s.Value;
+            }
+            return totalScore;
         }
+
     }
 }
--- GameTests/GameTests.cs
+++ GameTests/GameTests.cs
@@ -1,5 +1,6 @@
 using BowlingScore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace GameTests
 {
@@ -14,6 +15,43 @@ namespace GameTests
             var score = game.Score();
 
             Assert.AreEqual(0, score);
+        }
+
+        [TestMethod]
+        public void Score_Updates_Correctly_After_Roll_Is_Called()
+        {
+            int[] scoreset = { 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
+            var expectedScore = scoreset.Sum();
+            
+            var game = new Game();
+
+            foreach (var score in scoreset)
+            {
+                game.Roll(score);
+            }
+
+            var totalScore = game.Score();
+
+            Assert.AreEqual(expectedScore, totalScore);
+        }
+
+        [TestMethod]
+        public void Returns_Score_With_Spare_Bonus()
+        {
+            //index 4 of scoreset should trigger a spare bonus
+            int[] scoreset = { 2, 2, 2, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+            var expectedScore = scoreset.Sum() + 2;
+
+            var game = new Game();
+
+            foreach (var score in scoreset)
+            {
+                game.Roll(score);
+            }
+
+            var totalScore = game.Score();
+
+            Assert.AreEqual(expectedScore, totalScore);
         }
     }
 
